d6e1e60eb69d22d5ec03b4917d8f0e68b2c0bd8e
SkrzypMajster <skrzypmajster@gmail.com>
2019-04-08 21:49:04 +0200

    Install and implementation of moment.js library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
-    applicationName = 'Noodlee System';
+export class AppComponent implements OnDestroy {
+    applicationName: string;
+    timerObject: number;
+    currentDate: string;
 
-    currentDate = new Date().toISOString();
+    constructor() {
+        this.applicationName = 'Noodlee System';
+        this.currentDate = this.getCurrentDateString();
+
+        this.timerObject = window.setInterval( () => {
+            this.currentDate = this.getCurrentDateString();
+        }, 1000);
+    }
+
+    getCurrentDateString(): string {
+        return moment().format("DD-MM-YYYY HH:mm:ss");
+    }
+
+    ngOnDestroy(): void {
+        clearInterval(this.timerObject);
+    }
 }
