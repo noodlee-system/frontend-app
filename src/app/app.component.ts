a1cfcdfffae0ccb7feab308b48da183c4df89ca5
SkrzypMajster <skrzypmajster@gmail.com>
2019-04-08 21:31:59 +0200

    Prepare simple "Comming Soon" page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Noodlee System';
+    applicationName = 'Noodlee System';
+
+    currentDate = new Date().toISOString();
 }
