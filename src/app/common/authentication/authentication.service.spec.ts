53ca9b0391fde323d0c548e9fb75d70504d953e0
SkrzypMajster <skrzypmajster@gmail.com>
2019-04-12 23:24:47 +0200

    Fix unit tests

--- src/app/common/authentication/authentication.service.spec.ts
+++ src/app/common/authentication/authentication.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AuthenticationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [
+            HttpClientModule
+        ]
+    }));
 
-  it('should be created', () => {
-    const service: AuthenticationService = TestBed.get(AuthenticationService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: AuthenticationService = TestBed.get(AuthenticationService);
+        expect(service).toBeTruthy();
+    });
 });
