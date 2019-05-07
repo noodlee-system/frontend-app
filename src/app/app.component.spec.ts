3d330b945659d08b7a302de16927cf3dafc65125
SkrzypMajster <skrzypmajster@gmail.com>
2019-05-08 00:37:45 +0200

    Fix unit tests error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,12 +9,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', component: LoginPageComponent},
+    {path: 'register', component: RegisterPageComponent},
+    {path: '**', redirectTo: '/login'}
+];
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent,
+                LoginPageComponent,
+                LoginFormComponent,
                 RegisterFormComponent,
                 RegisterPageComponent,
                 FooterComponent,
@@ -23,6 +35,7 @@ describe('AppComponent', () => {
             imports: [
                 BrowserModule,
                 MaterialModule,
+                RouterModule.forRoot(appRoutes),
                 ReactiveFormsModule,
                 HttpClientModule,
                 FormsModule,
