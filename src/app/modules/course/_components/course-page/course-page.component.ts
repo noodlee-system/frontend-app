bdefcef0e3dd1a7475345bb5187aff1bf18ec758
SkrzypMajster <skrzypmajster@gmail.com>
2019-05-28 23:10:03 +0200

    Create CourseModule mock

---
+++ src/app/modules/course/_components/course-page/course-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-page',
+  templateUrl: './course-page.component.html'
+})
+export class CoursePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
